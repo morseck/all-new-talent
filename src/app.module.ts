b4a26ec5bb904944acc6032c050647395dcaa378
Mor SECK <mor0093@gmail.com>
2021-10-26 12:49:48 +0000

    Init Athlete module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AthleteModule } from './athlete/athlete.module';
 
 @Module({
-  imports: [],
+  imports: [AthleteModule],
   controllers: [AppController],
   providers: [AppService],
 })
